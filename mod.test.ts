e16d1e15cb76a6b6a598bf57e0bc73d9c1f34a6c
Nbiba Bedis <bedisnbiba@gmail.com>
2024-03-08 21:37:33 +0100

    result

---
+++ mod.test.ts
@@ -0,0 +1,34 @@
+import { Result } from "./mod.ts";
+import { assert, assertEquals } from "jsr:@std/assert";
+
+Deno.test("Result", () => {
+  {
+    const result = Result.wrap(() => Deno.readTextFileSync("./mod.ts"));
+    assert(result.isOk());
+    assertEquals(result.ok, Deno.readTextFileSync("./mod.ts"));
+  }
+  {
+    const result = Result.wrap<string, Deno.errors.NotFound>(() =>
+      Deno.readTextFileSync("doesnt-exist")
+    );
+    assert(result.isErr());
+    //@ts-ignore: code does exist
+    assertEquals(result.err.code, "ENOENT");
+  }
+});
+
+Deno.test("Async Result", async () => {
+  {
+    const result = await Result.wrap_async(() => Deno.readTextFile("./mod.ts"));
+    assert(result.isOk());
+    assertEquals(result.ok, Deno.readTextFileSync("./mod.ts"));
+  }
+  {
+    const result = await Result.wrap_async<string, Deno.errors.NotFound>(() =>
+      Deno.readTextFile("doesnt-exist")
+    );
+    assert(result.isErr());
+    //@ts-ignore: code does exist
+    assertEquals(result.err.code, "ENOENT");
+  }
+});
---
+++ mod.ts
@@ -0,0 +1,60 @@
+export class Result<T, E> {
+  #value?: T;
+  #error?: E;
+
+  constructor({ value, error }: { value: T; error: E }) {
+    this.#value = value;
+    this.#error = error;
+  }
+
+  get ok() {
+    return this.#value!;
+  }
+
+  get err() {
+    return this.#error!;
+  }
+
+  isOk() {
+    return this.#error === undefined;
+  }
+
+  isErr() {
+    return this.#error !== undefined;
+  }
+
+  expect(msg: string): T {
+    if (this.#error !== undefined) {
+      throw new Error(`${msg} - ${this.#error}`);
+    }
+    return this.#value!;
+  }
+
+  static wrap<T, E>(callback: () => T): Result<T, E> {
+    try {
+      return new Result({ value: callback(), error: undefined as E });
+      // deno-lint-ignore no-explicit-any
+    } catch (e: any) {
+      return new Result({ value: undefined as T, error: e as E });
+    }
+  }
+
+  static async wrap_async<T, E>(
+    callback: () => Promise<T>,
+  ): Promise<Result<T, E>> {
+    try {
+      return new Result({ value: await callback(), error: undefined as E });
+      // deno-lint-ignore no-explicit-any
+    } catch (e: any) {
+      return new Result({ value: undefined as T, error: e as E });
+    }
+  }
+}
+
+export function Ok<T, E>(value: T): Result<T, E> {
+  return new Result({ value, error: undefined as E });
+}
+
+export function Err<T, E>(error: E): Result<T, E> {
+  return new Result({ value: undefined as T, error });
+}
